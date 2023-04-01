d3c1d3b2bdbbb02ac8c7952b832b3b296268a6d6
jorgealll <117639889+jorgealll@users.noreply.github.com>
2023-04-01 11:33:59 +0200

    refactorizacion con mi nombre

--- tdd2/TestCoche.java
+++ tdd2/TestCoche.java
@@ -8,28 +8,28 @@ import org.junit.Test;
 public class TestCoche {
 
 	@Test
-    public void test_crear_coche_su_velocidad_es_cero(){
+	public void test_crear_coche_su_velocidad_es_cerojorgealtet(){
         Coche nuevoCoche = new Coche();
         Assert.assertEquals(0, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+    public void test_al_acelerar_un_coche_su_velocidad_aumentajorgealtet(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerarjorge(30);
        Assert.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+    public void test_al_decelerar_un_coche_su_velocidad_disminuyejorgealtet(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerarjorge(20);
         Assert.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_jorgealtet(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerarjorge(80);
         Assert.assertEquals(0, nuevoCoche.velocidad);
     }
 }
