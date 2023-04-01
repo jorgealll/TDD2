620e216ea4cf91c907a6ed231883a1da49f380ee
jorgealll <117639889+jorgealll@users.noreply.github.com>
2023-04-01 11:13:49 +0200

    Create TestCoche.java

---
+++ tdd2/TestCoche.java
@@ -0,0 +1,14 @@
+package tdd2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCoche {
+
+	@Test
+    public void test_crear_coche(){
+        Coche nuevoCoche = new Coche();
+    }
+
+}
