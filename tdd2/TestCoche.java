e5f166dc161c537978398fad98cd35b1e94a9513
jorgealll <117639889+jorgealll@users.noreply.github.com>
2023-04-01 11:21:21 +0200

    mejora del codigo

--- tdd2/TestCoche.java
+++ tdd2/TestCoche.java
@@ -1,14 +1,16 @@
 package tdd2;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 public class TestCoche {
 
 	@Test
-    public void test_crear_coche(){
+    public void test_crear_coche_su_velocidad_es_cero(){
         Coche nuevoCoche = new Coche();
+        Assert.assertEquals(0, nuevoCoche.velocidad);
     }
 
 }
