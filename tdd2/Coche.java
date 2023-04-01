cd12154ad7bf5c4c58525c6cdbc6fd28ac8b68d4
jorgealll <117639889+jorgealll@users.noreply.github.com>
2023-04-01 11:27:32 +0200

    coche completo

--- tdd2/Coche.java
+++ tdd2/Coche.java
@@ -2,4 +2,13 @@ package tdd2;
 
 public class Coche {
 public int velocidad;
+
+public void acelerar(int aceleracion) {
+    velocidad += aceleracion;
+}
+
+public void decelerar(int deceleracion) {
+    velocidad -= deceleracion;
+    if (velocidad<0) velocidad = 0;
+}
 }
